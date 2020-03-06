af6299cd009f6c42029215c6f135e54e296ba1ff
Shashang <51124608+shashang29@users.noreply.github.com>
2020-03-06 17:20:14 +0545

    performance improvement

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,11 @@
-import React, {PureComponent} from 'react';
+import React, { Component } from 'react';
 
-class Header extends PureComponent{
-    render(){
+class Header extends Component {
+    shouldComponentUpdate(nextProps, nextState) {
+        return false;
+    }
+    render() {
+        console.log('Header')
         return <h1 className='f1'>Robo Friends</h1>
     }
 }
