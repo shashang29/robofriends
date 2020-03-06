02f7a491eca4787ec8aa02910757c1399d3e5d2a
Shashang <51124608+shashang29@users.noreply.github.com>
2020-03-06 22:48:23 +0545

    activated service worker

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -3,7 +3,9 @@ import React from 'react';
 const SearchBox=({handleChange})=>{
     return (
         <div>
-            <input className='bg-light-blue ba b--red ma2 pa2 ' placeholder='Search Friends' type='search' onChange={handleChange}/>
+            <input
+            aria-label="Search Robots"
+            className='bg-light-blue ba b--red ma2 pa2 ' placeholder='Search Friends' type='search' onChange={handleChange}/>
 
         </div>
 
